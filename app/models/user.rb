8736ee9d51b55f931a2986150caa22193825d352
Marco Oquendo <marcooquendoc@gmail.com>
2023-01-19 17:32:05 -0500

    Create custom methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy, foreign_key: 'author_id'
   has_many :comments, dependent: :destroy, foreign_key: 'author_id'
   has_many :likes, dependent: :destroy, foreign_key: 'author_id'
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
