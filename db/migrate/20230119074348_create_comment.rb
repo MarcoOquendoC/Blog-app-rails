4c8cc087617b04c04fd6d243185899409252652b
Marco Oquendo <marcooquendoc@gmail.com>
2023-01-19 14:16:41 -0500

    Replace lines to make author the foreign key

--- db/migrate/20230119074348_create_comment.rb
+++ db/migrate/20230119074348_create_comment.rb
@@ -1,7 +1,7 @@
 class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :users, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: 'users' } 
       t.references :posts, null: false, foreign_key: true
       t.text :text
 
--- db/migrate/20230119074426_create_like.rb
+++ db/migrate/20230119074426_create_like.rb
@@ -1,7 +1,7 @@
 class CreateLike < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :users, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: 'users' }
       t.references :posts, null: false, foreign_key: true
 
       t.timestamps
