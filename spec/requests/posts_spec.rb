d16cf19ef0bd2ccf35a793430ab7c856dbc868ba
Marco Oquendo <marcooquendoc@gmail.com>
2023-01-25 18:42:50 -0500

    Update rspec tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/posts/index'
+      get '/users/:id/posts'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/posts/show'
+      get '/users/:id/posts/:id'
       expect(response).to have_http_status(:success)
     end
   end
