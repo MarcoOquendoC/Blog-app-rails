25787540399bbd3d53d586b625ecb917367516ae
Marco Oquendo <marcooquendoc@gmail.com>
2023-02-04 14:24:19 -0500

    Update login with email to be username or email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,26 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable,
+         authentication_keys: [:login]
+
   has_many :posts, dependent: :destroy, foreign_key: 'author_id'
   has_many :comments, dependent: :destroy, foreign_key: 'author_id'
   has_many :likes, dependent: :destroy, foreign_key: 'author_id'
 
-  # validates :name, presence: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :name, presence: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
   # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  attr_accessor :login
+
+  def login
+    @login || self.name || self.email
+  end
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
@@ -16,5 +28,16 @@ class User < ApplicationRecord
 
   def posts_list
     posts.order(created_at: :desc)
+  end
+
+  private
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["name = :value OR lower(email) = lower(:value)", { value: login }]).first
+    elsif conditions.has_key?(:name) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
   end
 end
