e66b03b37fbb8b1d66930ede3bfeda136a753643
Marco Oquendo <marcooquendoc@gmail.com>
2023-02-04 17:53:04 -0500

    Update views in devise. Add styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy, foreign_key: 'author_id'
 
   validates :name, presence: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
-  # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   attr_writer :login
 
